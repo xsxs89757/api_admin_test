cb2570564949236dee5d6bd92f7f6b4577e3b591
王富贵儿 <317448423@qq.com>
2019-03-22 13:26:05 +0800

    注册测试

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title','关于我们')
+@section('content')
+    <div class="jumbotron">
+        <h1>关于 Laravel</h1>
+    </div>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+    <div class="jumbotron">
+        <h1>帮助 Laravel</h1>
+    </div>
+@stop
